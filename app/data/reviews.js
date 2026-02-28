a1ac4cfcd664a49203bb75374fd494c7ecad4ba0
scmeadllc <scmeadllc@gmail.com>
2026-02-28 15:18:33 -0800

    Update reviews.js

--- app/data/reviews.js
+++ app/data/reviews.js
@@ -1,106 +1,120 @@
-export const allReviews = [
- {
-  id: "yelp-brett-m-2026-02-27",
-  name: "Brett M.",
-  location: "Beresford Park, San Mateo, CA",
-  source: "Yelp",
-  rating: 5,
-  date: "2026-02-27",
-  text:
-    "I needed an access point installed in our house that had an addition. I was able to do most of the Ethernet cables in the house but due to the addition I was not able to get the Ethernet cables into the newer part of the house. I had posted in the Lemon Valley community on Facebook but got some completely ridiculous replies. Shane said he would like to come take a look and provide a quote. Shane arrived on time, quoted the job and explained his experience. After talking with him I also had him relocate some items that Spectrum said could not be moved. Shane's work was fantastic. He was able to run the Ethernet cables, mounted the access point and was able to relocate the Spectrum boxes in a hall closet. Shane was very professional and his quoted price was the final price. I highly recommend Shane and will use him for future work!"
-}
-  
-  // Angi reviews
+export const reviews = [
+  // Yelp Review (Featured)
   {
+    id: "yelp-brett-m-2026-02-27",
+    source: "Yelp",
+    name: "Brett M.",
+    location: "Beresford Park, San Mateo, CA",
+    rating: 5,
+    date: "2026-02-27",
+    text:
+      "I needed an access point installed in our house that had an addition. I was able to do most of the Ethernet cables in the house but due to the addition I was not able to get the Ethernet cables into the newer part of the house. Shane arrived on time, quoted the job and explained his experience. He ran the Ethernet cables, mounted the access point and relocated the Spectrum boxes cleanly. Shane was very professional and his quoted price was the final price. I highly recommend Shane and will use him for future work!",
+  },
+
+  // Angi Reviews
+  {
+    id: "angi-robert-e",
     source: "Angi",
     name: "Robert E.",
-    city: "Sparks, NV",
+    location: "Sparks, NV",
     rating: 5,
-    review:
+    text:
       "Shane did a great job. I had him extend my network in my home. He was professional and considerate while completing the work.",
   },
   {
+    id: "angi-ruell-m",
     source: "Angi",
     name: "Ruell M.",
-    city: "Sparks, NV",
+    location: "Sparks, NV",
     rating: 5,
-    review: "Shane is excellent, prompt, professional and great quality.",
+    text: "Shane is excellent, prompt, professional and great quality.",
   },
   {
+    id: "angi-joe-c",
     source: "Angi",
     name: "Joe C.",
-    city: "Reno, NV",
+    location: "Reno, NV",
     rating: 5,
-    review:
+    text:
       "Quick, friendly. Helped with an additional issue and it made a huge difference! Would recommend!!",
   },
   {
+    id: "angi-lonnie-r",
     source: "Angi",
     name: "Lonnie R.",
-    city: "Sparks, NV",
+    location: "Sparks, NV",
     rating: 5,
-    review: "He got the job without calling people.",
+    text: "He got the job done without hassle.",
   },
   {
+    id: "angi-grant-s",
     source: "Angi",
     name: "Grant S.",
-    city: "Sparks, NV",
+    location: "Sparks, NV",
     rating: 5,
-    review: "Good work and efficient.",
+    text: "Good work and efficient.",
   },
   {
+    id: "angi-rafael-p",
     source: "Angi",
     name: "Rafael P.",
-    city: "Reno, NV",
+    location: "Reno, NV",
     rating: 5,
-    review: "Did a great job. Awesome work, would highly recommend.",
+    text: "Did a great job. Awesome work, would highly recommend.",
   },
   {
+    id: "angi-jody-g",
     source: "Angi",
     name: "Jody G.",
-    city: "Reno, NV",
+    location: "Reno, NV",
     rating: 5,
-    review:
-      "Shane did a fantastic job hanging many TVs and the associated sound bars throughout our new home. He is very professional and highly conscientious of the placement aesthetics including articulation, balance, and hiding wires. He answered all our questions and cleaned up as if he was never here.",
+    text:
+      "Shane did a fantastic job hanging many TVs and sound bars throughout our new home. Very professional and highly conscientious about placement aesthetics and hiding wires.",
   },
   {
+    id: "angi-brent-r",
     source: "Angi",
     name: "Brent R.",
-    city: "Reno, NV",
+    location: "Reno, NV",
     rating: 5,
-    review:
-      "Provided the services as they were originally discussed. Arrived promptly and was very professional and courteous.",
+    text:
+      "Provided the services as originally discussed. Arrived promptly and was very professional and courteous.",
   },
   {
+    id: "angi-steve-a",
     source: "Angi",
     name: "Steve A.",
-    city: "Reno, NV",
+    location: "Reno, NV",
     rating: 5,
-    review: "They did a great job! Highly recommend them!!!",
+    text: "They did a great job! Highly recommend them!!!",
   },
   {
+    id: "angi-jerry-h",
     source: "Angi",
     name: "Jerry H.",
-    city: "Reno, NV",
+    location: "Reno, NV",
     rating: 5,
-    review:
-      "Shane is knowledgeable and professional. He came by to assess the work beforehand, was punctual, and very clear about his estimated costs.",
+    text:
+      "Shane is knowledgeable and professional. He came by to assess the work beforehand, was punctual, and very clear about estimated costs.",
   },
-  // Google reviews
+
+  // Google Reviews
   {
+    id: "google-paige-b",
     source: "Google",
     name: "Paige B.",
-    city: "Reno, NV",
+    location: "Reno, NV",
     rating: 5,
-    review:
-      "Highly recommend! Sean did a great job for us. He was fast, efficient, and insisted on cleaning up all the mess left behind from running lines. Very happy we found him.",
+    text:
+      "Highly recommend! Shane did a great job. He was fast, efficient, and cleaned up everything after running lines.",
   },
   {
+    id: "google-munro-bonnell",
     source: "Google",
     name: "Munro M. Bonnell",
-    city: "Reno, NV",
+    location: "Reno, NV",
     rating: 5,
-    review:
-      "Shane’s services were top notch! He assessed my network, gave me options, completed the work cleanly, communicated throughout, and verified full functionality. His installation of Ethernet wall plugs was super clean and professional. I highly recommend him!",
+    text:
+      "Shane assessed my network, gave options, completed the work cleanly, and verified full functionality. His Ethernet wall plug installs were super clean and professional.",
   },
 ];
