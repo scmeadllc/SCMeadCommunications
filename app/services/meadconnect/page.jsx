d517bb247dcf4b2df95bb9c67a54503c9bcff19e
scmeadllc <scmeadllc@gmail.com>
2026-07-12 13:36:59 -0700

    Update page.jsx

--- app/services/meadconnect/page.jsx
+++ app/services/meadconnect/page.jsx
@@ -1,7 +1,6 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
-
 export const metadata = {
   title: "MeadConnect Business Phone Systems | S. C. Mead Communications",
   description:
